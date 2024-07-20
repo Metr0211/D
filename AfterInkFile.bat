4e1548d3c625e5c18e9558565c8a2a2abffea74a
DDDDDDDDDDDDDDDDDDDDDDDDDDD <147281421+Metr0211@users.noreply.github.com>
2024-07-19 18:57:57 -0700

    Delete AfterInkFile.bat

--- AfterInkFile.bat
+++
@@ -1,7 +0,0 @@
-@Echo off
-powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Starting Testing Phase Graphic "Drag Results.txt"', 'TimerUI', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);"  
-    Pause >Nul
-    start https://chart-studio.plotly.com/create/#/
-powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('AutoMode Is Implemented NOW.', 'TimerRes', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
-cls
-goto CustomValue
